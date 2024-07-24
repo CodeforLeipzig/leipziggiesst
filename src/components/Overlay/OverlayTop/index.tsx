f8f13cb306829a5e8d0f862c334b3d0313354e3b
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2024-07-24 08:36:02 +0200

    es gibt (r)eis, baby

--- src/components/Overlay/OverlayTop/index.tsx
+++ src/components/Overlay/OverlayTop/index.tsx
@@ -61,12 +61,12 @@ const OverlayTop: FC = () => {
         <OverlayTitle size='xxl' title={title} />
         <img style={{ height: 160 }} src='images/leipzig-giesst-logo.png' />
         {!isMobile && (<div style={{ width: '60%', fontSize: '16pt', fontStyle: 'bold', color: 'blue' }}>
-          Kommender Termin: <a style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={() => { history.push("/event/20240723_teamtreffen")}}>Am Dienstag, den 23. Juli, 19:00, Teamtreffen ins Ilses Erika.</a><br /><br />Alle Gießevents findet ihr ab sofort <a style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={() => { history.push("/events")}}>links in der Seitenleiste</a> und auf der Karte.
+          Kommender Termin: Dienstag, den 6. August, wollen ab 18 Uhr bei <a style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={() => { history.push("/event/20240806_manusso_es_gibt_eis_baby")}}>Manusso</a> wieder die IBC-Container ausgiebig nutzen und sind als Leipzig-Giesst-Team vor Ort. Kommt gerne vorbei.<br /><br />Alle Gießevents findet ihr ab sofort <a style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={() => { history.push("/events")}}>links in der Seitenleiste</a> und auf der Karte.
         </div>)}
       </Wrapper>
       <OverlayTitle size='small' title={""} />
       {isMobile && (<div style={{ paddingLeft: '40px', paddingBottom: '20px', width: '60%', fontSize: '16pt', fontStyle: 'bold', color: 'blue' }}>
-        Kommender Termin: <a style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={() => { history.push("/event/20240723_teamtreffen")}}>Am Dienstag, den 23. Juli, 19:00, Teamtreffen ins Ilses Erika.</a><br /><br />Alle Gießevents findet ihr ab sofort <a style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={() => { history.push("/events")}}>links in der Seitenleiste</a> und auf der Karte.
+        Kommender Termin: Dienstag, den 6. August, wollen ab 18 Uhr bei <a style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={() => { history.push("/event/20240806_manusso_es_gibt_eis_baby")}}>Manusso</a> wieder die IBC-Container ausgiebig nutzen und sind als Leipzig-Giesst-Team vor Ort. Kommt gerne vorbei.<br /><br />Alle Gießevents findet ihr ab sofort <a style={{ cursor: 'pointer', textDecoration: 'underline' }} onClick={() => { history.push("/events")}}>links in der Seitenleiste</a> und auf der Karte.
         </div>)}
       <OverlayTitle size='xxl' title={subline} />
       {isMobile && <OverlayTitle size='medium' title={disclaimer} />}
