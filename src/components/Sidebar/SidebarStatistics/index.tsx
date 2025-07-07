ef2bf741e590a64e4b710e3bbd7a2c02a1aa8961
Jörg Reichert <joergreichert@users.noreply.github.com>
2025-07-07 22:19:40 +0200

    update grafana link

--- src/components/Sidebar/SidebarStatistics/index.tsx
+++ src/components/Sidebar/SidebarStatistics/index.tsx
@@ -30,7 +30,7 @@ const SidebarStatistics: FC = () => {
           <SmallParagraph>{((user.pos < 10) ? '00' : ((user.pos < 100) ? '0' : '')) + user.pos}. {user.username} ({user.liter} Liter)</SmallParagraph>
         ))}
         <Paragraph>
-          Mehr Statistiken gibt es <a target="_blank" href="https://joergreichert.github.io/gdk-opendata-plot/">hier</a> und <a target="_blank" href="https://dashboard.codeforleipzig.de/d/RQFRCgeGk/leipziggiesstpublic?orgId=3&from=1709247600000&to=now">hier</a>.
+          Mehr Statistiken gibt es <a target="_blank" href="https://joergreichert.github.io/gdk-opendata-plot/">hier</a> und <a target="_blank" href="https://dashboard.codeforleipzig.de/d/RQFRCgeGk/leipziggiesstpublic?orgId=2&from=1709247600000&to=now">hier</a>.
         </Paragraph>
       </FlexCol>
     </>
